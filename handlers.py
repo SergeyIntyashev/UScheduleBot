e0d37de55367c133a70d6f6ff88927f8c6e0dfb3
Sergey Intyashev <seintyashev@mail.ru>
2022-09-12 15:34:11 +0400

    Add /today handler. Additional docstring

--- handlers.py
+++ handlers.py
@@ -1,9 +1,13 @@
 from aiogram import types
 
+import services
+from models import Week
+
 
 async def send_welcome(message: types.Message):
     """
     Данный хендлер вызывается, когда пользователь отправляет команду /start
+    Отправляет сообщение-приветствие
     """
     await message.answer("Привет!\nЯ бот помогающий тебе не пропустить пары!\n"
                          "Я автоматически буду отправлять тебе напоминание о парах"
@@ -13,7 +17,22 @@ async def send_welcome(message: types.Message):
 async def send_help(message: types.Message):
     """
     Данный хендлер вызывается, когда пользователь отправляет команду /help
+    Отправляет сообщение со списком доступных команд
     """
     await message.answer("Расписание на текущий день /today\n"
                          "Расписание на текущую неделю /current\n"
                          "Расписание на cледующую неделю /next\n")
+
+
+async def schedule_today(message: types.Message):
+    """
+    Хендлер вызывается, когда пользователь отправляет команду /today
+    Отправляет сообщение с расписанием на сегодняшний день
+    """
+
+    classes_info = await services.get_week_schedule(Week.CURRENT)
+
+    schedule = services.get_schedule_today(classes_info)
+
+    await message.answer(schedule)
+
