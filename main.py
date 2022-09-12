da64c604eb7bc68145f602713539e2bedab2039c
Sergey Intyashev <seintyashev@mail.ru>
2022-09-12 15:21:02 +0400

    Init launch bot and connect start and help commands

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+
+from aiogram import Bot, Dispatcher, executor
+from dotenv import load_dotenv
+
+import handlers
+
+load_dotenv()
+
+API_TOKEN = os.environ.get('API_TOKEN')
+
+bot = Bot(token=API_TOKEN)
+
+dp = Dispatcher(bot)
+
+dp.register_message_handler(handlers.send_welcome, commands=["start"])
+
+dp.register_message_handler(handlers.send_help, commands=["help"])
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
