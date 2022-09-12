8540452e9f1575a9bcea65a5fe8d2cfa072b4158
Sergey Intyashev <seintyashev@mail.ru>
2022-09-12 18:28:50 +0400

    Add keyboards

---
+++ keyboards.py
@@ -0,0 +1,12 @@
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+
+today_button = KeyboardButton('Расписание на текущий день')
+current_week_button = KeyboardButton('Расписание на текущую неделю')
+next_week_button = KeyboardButton('Расписание на следующую неделю')
+
+keyboard_client = ReplyKeyboardMarkup()
+
+keyboard_client. \
+    add(today_button). \
+    add(current_week_button). \
+    add(next_week_button)
