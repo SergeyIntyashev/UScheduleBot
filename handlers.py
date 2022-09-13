7ec3eef57c6fbba26a670d5893a293e05dd4e980
Sergey Intyashev <seintyashev@mail.ru>
2022-09-13 10:49:43 +0400

    Add handlers for remind and not remind

--- handlers.py
+++ handlers.py
@@ -2,6 +2,7 @@ from aiogram import types, Dispatcher
 from aiogram.types import ParseMode
 
 import services
+from db import db_helper
 from keyboards import keyboard_client
 from models import Week
 
@@ -26,7 +27,10 @@ async def send_help(message: types.Message):
     """
     await message.answer("Расписание на текущий день /today\n"
                          "Расписание на текущую неделю /current\n"
-                         "Расписание на cледующую неделю /next\n")
+                         "Расписание на cледующую неделю /next\n"
+                         "Напоминать о парах /subscribe\n"
+                         "Не напоминать о парах /unsubscribe\n"
+                         )
 
 
 async def send_today_schedule(message: types.Message):
@@ -62,6 +66,30 @@ async def send_next_week_schedule(message: types.Message):
     await message.answer(schedule, parse_mode=ParseMode.HTML)
 
 
+async def remind_schedule(message: types.Message):
+    """Добавляет пользователя в БД для отправки напоминания о парах"""
+
+    user_id = message.from_user.id
+
+    if db_helper.user_exist(user_id):
+        return await message.answer('Вы уже добавлены в рассылку')
+
+    db_helper.add_user(user_id)
+    await message.answer('Вы добавлены в рассылку')
+
+
+async def not_remind_schedule(message: types.Message):
+    """Удаляет пользователя из БД, чтобы не получать напоминания о парах"""
+
+    user_id = message.from_user.id
+
+    if not db_helper.user_exist(user_id):
+        return await message.answer('Вы не подписаны на рассылку')
+
+    db_helper.delete_user(user_id)
+    await message.answer('Вы удалены из рассылки')
+
+
 async def handle_message(message: types.Message):
     """
     Хендлер вызывается, когда пользователь отправляет боту сообщение
@@ -74,6 +102,10 @@ async def handle_message(message: types.Message):
             await send_current_week_schedule(message)
         case 'Расписание на следующую неделю':
             await send_next_week_schedule(message)
+        case 'Напоминать о парах':
+            await remind_schedule(message)
+        case 'Не напоминать о парах':
+            await not_remind_schedule(message)
         case _:
             await message.reply('Извините, такую команду я не могу обработать '
                                 ':(\n'
@@ -89,4 +121,6 @@ def register_handlers(dp: Dispatcher) -> None:
     dp.register_message_handler(send_current_week_schedule,
                                 commands=['current'])
     dp.register_message_handler(send_next_week_schedule, commands=['next'])
+    dp.register_message_handler(remind_schedule, commands=['subscribe'])
+    dp.register_message_handler(not_remind_schedule, commands=['unsubscribe'])
     dp.register_message_handler(handle_message)
