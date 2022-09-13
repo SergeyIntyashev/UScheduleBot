9a37e2da99d30f13e9600b30f1c8cb74bddbd6a9
Sergey Intyashev <seintyashev@mail.ru>
2022-09-13 10:38:53 +0400

    Add remind and not remind button

--- keyboards.py
+++ keyboards.py
@@ -3,10 +3,14 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 today_button = KeyboardButton('Расписание на текущий день')
 current_week_button = KeyboardButton('Расписание на текущую неделю')
 next_week_button = KeyboardButton('Расписание на следующую неделю')
+remind_button = KeyboardButton('Напоминать о парах')
+not_remind_button = KeyboardButton('Не напоминать о парах')
 
 keyboard_client = ReplyKeyboardMarkup(resize_keyboard=True)
 
 keyboard_client. \
     add(today_button). \
     add(current_week_button). \
-    add(next_week_button)
+    add(next_week_button). \
+    add(remind_button). \
+    add(not_remind_button)
